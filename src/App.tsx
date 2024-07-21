1399b423ad5b224619ac856f22f38dcfcc16fa08
CharlesGabin <charlesgabinnemecksongna@gmail.com>
2024-07-21 10:15:21 +0530

    resolve problem for deployment and update react-helmet to 6.1.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Product from "./components/Product";
 import { Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Micki Cakes - Le plaisir avec grace</title>
+        <link rel="icon" href="/hero-bg.jpg" />
+      </Helmet>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
