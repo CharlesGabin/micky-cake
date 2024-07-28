8aaa506502fb003537a20fed63f5f16f3890af61
CharlesGabin <charlesgabinnemecksongna@gmail.com>
2024-07-28 07:07:27 +0530

    finalisation

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,7 @@ function App() {
   const WA_URL = "https://api.whatsapp.com/send?phone=";
   return (
     <main className="relative flex flex-col min-h-screen antialiased">
-      <Helmet>
-        <title>Micki Cakes - Le plaisir avec grace</title>
-        <link rel="icon" type="image/jpg" href="/hero-bg.jpg" />
-        <meta
-          property="og:title"
-          content="Micki Cakes - Le plaisir avec grace"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/hero-bg.jpg" />
-        <meta property="og:url" content="https://micky-cake.vercel.app/" />
-        <meta name="robots" content="index, follow" />
-      </Helmet>
+      <Helmet></Helmet>
       <div className="sticky top-[80%] left-0 z-50 pl-4 ">
         <a href={WA_URL + waNumber} target="_blank">
           <SocialIcon network="whatsapp"></SocialIcon>
